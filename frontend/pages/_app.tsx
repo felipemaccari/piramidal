dfeddd542c5808cd5bc960359a6ccf07b07d203b
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-01-31 00:37:01 -0300

    feat(frontend): create frontend with navbar

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,23 @@
+import type { AppProps } from "next/app";
+
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  primary: "blue",
+  secondary: "green",
+  grey: "#ccc",
+  black: "#111",
+  white: "#eee",
+};
+
+const theme = extendTheme({ colors });
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
