6210cb5354fdd8bd5662f02aae2c8276835292bd
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-05-02 22:08:25 -0300

    doc(swagger): add swagger base config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { logger } from "../utils/logger";
 import router from "./routes";
+import swaggerFile from "./swagger.json";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(process.env.PORT, () =>
