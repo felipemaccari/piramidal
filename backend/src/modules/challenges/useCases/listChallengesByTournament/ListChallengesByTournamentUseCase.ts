9e2c9050fab6335b8a7ae521f003f295d7f0c4c9
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-10-24 20:47:24 -0300

    feat: add players name on challenge by tournament

--- backend/src/modules/challenges/useCases/listChallengesByTournament/ListChallengesByTournamentUseCase.ts
+++ backend/src/modules/challenges/useCases/listChallengesByTournament/ListChallengesByTournamentUseCase.ts
@@ -2,12 +2,15 @@ import { inject, injectable } from "tsyringe";
 
 import Challenge from "@modules/challenges/infra/typeorm/entities/Challenge";
 import { IChallengesRepository } from "@modules/challenges/repositories/IChallengesRepository";
+import { IPlayersRepository } from "@modules/players/repositories/IPlayersRepository";
 
 @injectable()
 class ListChallengesByTournamentUseCase {
   constructor(
     @inject("ChallengesRepository")
-    private challengeRepository: IChallengesRepository
+    private challengeRepository: IChallengesRepository,
+    @inject("PlayersRepository")
+    private playersRepository: IPlayersRepository
   ) {}
 
   async execute(tournamentID: string): Promise<Challenge[]> {
@@ -15,7 +18,25 @@ class ListChallengesByTournamentUseCase {
       tournamentID
     );
 
-    return challenges;
+    const players = await this.playersRepository.list();
+
+    const challengesWithPlayerInformation = challenges.map((challenge) => {
+      const originPlayerInformation = players.find(
+        (player) => player.id === challenge.originPlayerID
+      );
+
+      const destinationPlayerInformation = players.find(
+        (player) => player.id === challenge.destinationPlayerID
+      );
+
+      return {
+        ...challenge,
+        originPlayerName: originPlayerInformation.name,
+        destinationPlayerName: destinationPlayerInformation.name,
+      };
+    });
+
+    return challengesWithPlayerInformation;
   }
 }
 
